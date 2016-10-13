4b2ce1329d93593d949586e7c53d4a82bfc54cc3
dvdpwll <dvdpwll456@gmail.com>
2016-10-12 21:08:20 -0400

    deploying to heroku

--- app/middleware/mongoose.js
+++ app/middleware/mongoose.js
@@ -1,7 +1,12 @@
 'use strict';
 
 const mongoose = require('mongoose');
-const uri = process.env.MONGOLAB_URI || 'mongodb://localhost/CRUD-Bucket';
+let uri;
+if (process.env.NODE_ENV === 'production') {
+  uri = process.env.MONGODB_URI;
+} else {
+  uri = 'mongodb://localhost/CRUD-Bucket';
+}
 mongoose.Promise = global.Promise;
 mongoose.connect(uri);
 
